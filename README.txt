2d9c8030f5fb805f2803eb9dbca781acfaa16cba
y7t9yn4hz8-cloud <y7t9yn4hz8@privaterelay.appleid.com>
2026-09-08 11:06:47 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+Depths of Veyra — PWA Prototype 0.2
+
+This package is ready to be hosted as a small static website.
+Files:
+- index.html
+- manifest.webmanifest
+- sw.js
+- app icons
+
+On iPhone, after it is hosted:
+1. Open the site in Safari.
+2. Tap Share.
+3. Tap Add to Home Screen.
+4. Launch Veyra from its icon.
+
+The game saves locally on the device/browser using localStorage.
---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="veyra-v0.2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
