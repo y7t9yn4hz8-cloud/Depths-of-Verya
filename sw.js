6023e667221f84f9a8c029a89f126ac8c40f5e0e
y7t9yn4hz8-cloud <y7t9yn4hz8@privaterelay.appleid.com>
2026-09-10 01:05:41 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="veyra-v0.3.1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
+const CACHE="veyra-v0.4.0";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./Floor_01_Game_Map_53_Doors.png","./Floor_01_Tilemap_53_Doors.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
